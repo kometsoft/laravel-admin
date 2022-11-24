b37a02cca1aeaa209d803b47db22ec710e04b755
syd <sayed@kometsoft.com.my>
2022-11-24 14:23:39 +0800

    feat

--- src/AdminServiceProvider.php
+++ src/AdminServiceProvider.php
@@ -14,8 +14,8 @@ class TablerServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__ . '/config/admin.php',
-            'admin'
+            __DIR__ . '/config/laravel-admin.php',
+            'laravel-admin'
         );
     }
 
@@ -28,10 +28,14 @@ class TablerServiceProvider extends ServiceProvider
     {
         $this->publishes([
             // Config file
-            __DIR__ . '/config/admin.php' => config_path('admin.php'),
+            __DIR__ . '/config/laravel-admin.php' => config_path('laravel-admin.php'),
 
             // Stubs
-            __DIR__ . '/../stubs/resources/views' => resource_path('views'),
+            __DIR__ . '/../stubs/app' => base_path('app'),
+            __DIR__ . '/../stubs/database' => base_path('database'),
+            __DIR__ . '/../stubs/resources/views' => base_path('resources/views'),
+            __DIR__ . '/../stubs/routes' => base_path('routes'),
+            __DIR__ . '/../stubs/stubs' => base_path('stubs'),
         ], 'laravel-admin');
     }
 }
